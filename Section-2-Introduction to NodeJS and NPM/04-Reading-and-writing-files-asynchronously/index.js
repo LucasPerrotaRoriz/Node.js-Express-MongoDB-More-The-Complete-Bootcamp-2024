ff2686a26f07ff2f698f7c4d2d1d77a97210378d
LucasSensei <Lucaswebdeveloper@proton.me>
2024-04-03 15:32:00 -0300

    Reading and writing async

---
+++ Section-2-Introduction to NodeJS and NPM/04-Reading-and-writing-files-asynchronously/index.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+// Blocking, synchronous way
+/* 
+    const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+    console.log(textIn);
+
+    const textOut = `This is what we know about the avocado: ${textIn}.
+    Created on ${Date.now()}`;
+    fs.writeFileSync('./txt/output.txt', textOut);
+    console.log('File written');
+*/
+
+// Non-Blocking, asynchronous way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    if(err) return console.log('ERROR! ', err);
+
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        if(err) return console.log('ERROR! ', err);
+        console.log(data2);
+
+        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+            if(err) return console.log('ERROR! ', err);
+            console.log(data3);
+            
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('File has been written!');
+            })
+        })
+    })
+});
+console.log('Will read file!');
+
+
+
+
+
+
+
+
