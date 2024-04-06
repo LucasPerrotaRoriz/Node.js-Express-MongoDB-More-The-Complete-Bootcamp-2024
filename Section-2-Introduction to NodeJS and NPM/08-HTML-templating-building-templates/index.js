4fe3d16bf8eab14c7ae7511233cf1420b7a271e0
LucasSensei <Lucaswebdeveloper@proton.me>
2024-04-05 21:52:24 -0300

    Building templates

---
+++ Section-2-Introduction to NodeJS and NPM/08-HTML-templating-building-templates/index.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const productData = JSON.parse(data);
+
+http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, {
+        'Content-type': 'application/json',
+        "my-own-header": "hello-world",
+    })
+    res.end(data)
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page Not Found!</h1>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
